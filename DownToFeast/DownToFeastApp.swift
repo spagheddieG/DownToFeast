e352f448563dffccde0a460431cbadbc20b673d5
spagheddieg <eddie@dailies.co>
2025-04-02 10:55:30 -0500

    Initial Commit

---
+++ DownToFeast/DownToFeastApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownToFeastApp.swift
+//  DownToFeast
+//
+//  Created by Eddie Gomez on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DownToFeastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
